b625d8d9a647eb4f9e1b5e21e5e34b89fce43601
salcedoe <esalcedo94@yahoo.com>
2019-12-23 22:05:07 -0800

    Finished! Added functionality to cards

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -18,36 +18,39 @@ var cards = [
     {
         rank: "king",
         suit: "diamonds",
-        cardImage: "images/kings-of-diamonds.png"
-    },
-    
-    
-    
+        cardImage: "images/king-of-diamonds.png"
+    }, 
 ];
 var cardsInPlay = [];
 
 function checkForMatch(){
 
     if(cardsInPlay[0] === cardsInPlay[1]){
-        console.log("You found a match!");
+        alert("You found a match!");
     }
     else{
-        console.log("Sorry try again");
+        alert("Sorry try again");
     }
     
 }
-function flipCard(cardId){
-    console.log("User flipped " + cards[cardId].rank);
-    console.log("Card image " + cards[cardId].cardImage);
-    console.log("Suit " + cards[cardId].suit)
+function flipCard(){
+    var cardId = this.getAttribute("data-id");
     cardsInPlay.push(cards[cardId].rank);
-
+    this.setAttribute("src", cards[cardId].cardImage);
     if(cardsInPlay.length === 2){
         checkForMatch();
     }
 }
-flipCard(0);
-flipCard(2);
+function createBoard(){
+    for(let i = 0; i < cards.length; i++){
+        var cardElement = document.createElement("img");
+        cardElement.setAttribute("src","images/back.png");
+        cardElement.setAttribute("data-id", i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById("game-board").appendChild(cardElement);
+    }
+}
+createBoard();
 
 
 
